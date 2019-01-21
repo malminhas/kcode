f78a3479577b63cc509fb3d1167979c07b658ce9
Anzel Lai <anzel_lai@hotmail.com>
2019-01-21 19:57:14 +0000

    refactor: split main code into smaller composable and testable functions

--- main.go
+++ main.go
@@ -1,182 +1,134 @@
 package main
 
 import (
-    "encoding/json"
+	"encoding/json"
+	"errors"
 	"fmt"
-    "net/http"
+	"net/http"
+
 	kcode "github.com/KanoComputing/go/kcode"
-    
+	log "github.com/sirupsen/logrus"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type resp struct {
-    Method  string `json:"method"`
-    Body  string `json:"data"`
-    Count int `json:"count"`
+const (
+	// handlerName for logging purposes
+	handlerName = "KCode Web Service"
+	// sub method constants
+	spellsMethod   = "spells"
+	blocksMethod   = "blocks"
+	validateMethod = "validate"
+)
+
+var (
+	logger = log.WithField("handler", handlerName)
+)
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
 }
 
-func dumpHTTPRequest(req events.APIGatewayProxyRequest) {
-	method := req.HTTPMethod
-	params := req.QueryStringParameters
-	hdrs := req.Headers
-	body := req.Body
-	b64 := req.IsBase64Encoded
-	fmt.Printf("::dumpHTTPRequest::\n\t%s\n\tHeaders=%s\n\tQueryStringParameters=%s\n\tBody=%s (%d bytes)\n\tIsBase64:%t\n",
-	    method,hdrs,params,body,len(body),b64)
+// Request is an alias to APIGatewayProxyRequest type
+type Request = events.APIGatewayProxyRequest
+
+// Response is an alias to the APIGatewayProxyResponse
+type Response = events.APIGatewayProxyResponse
+
+// Payload is the struct of expected payload body from request
+type Payload struct {
+	Method string `json:"method"`
+	Body   string `json:"data"`
+	Count  int    `json:"count"`
+}
+
+func makeResponse(status int, err error, result interface{}) (Response, error) {
+	response := Response{
+		StatusCode: status,
+		Body:       http.StatusText(status),
+	}
+	// If there is any errors, include error string representation into the response Body
+	if err != nil {
+		response.Body = fmt.Sprintf("%s - %v", response.Body, err)
+	} else {
+		body, err := json.Marshal(result)
+		if err != nil {
+			response.Body = fmt.Sprintf("internal result response error  - %v", err)
+		} else {
+			response.Body = string(body)
+		}
+	}
+	return response, nil
+}
+
+func validateSubMethod(method string) error {
+	switch method {
+	case spellsMethod, blocksMethod, validateMethod:
+		return nil
+	default:
+		return fmt.Errorf("allowed methods are: speels, blocks or validate")
+	}
+}
+
+// validatePayload validate the payload in string and return a parsed payload bytes alongside any error
+func validatePayload(body string) ([]byte, error) {
+	payload := Payload{}
+	if err := json.Unmarshal([]byte(body), &payload); err != nil {
+		return nil, errors.New("invalid JSON payload")
+	}
+	// marshal payload back to JSON bytes for use in kcode.METHODS
+	out, err := json.Marshal(payload)
+	if err != nil {
+		return nil, fmt.Errorf("internal Payload struct error (%v)", err)
+	}
+	return out, nil
+}
+
+// kcodeHandler will parse input payload and return either spells or blocks
+func kcodeHandler(input []byte, submethod string) []string {
+	kc := kcode.ExtractKcode(input)
+	spells, blocks := kcode.ProcessKcodeString(kc, submethod == spellsMethod, submethod == blocksMethod, true)
+	switch submethod {
+	case spellsMethod:
+		return spells
+	case blocksMethod:
+		return blocks
+	default:
+		return []string{}
+	}
 }
 
 // router is executed by AWS Lambda in the main function and returns an Amazon API Gateway response object to AWS Lambda.
-func router(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	dumpHTTPRequest(req)
-    method := req.QueryStringParameters["method"]
-    switch req.HTTPMethod {
-	case "GET":
-		return testGetRequest(req)
-    case "POST":
-		switch method {
-		case "spells":
-			return findBlocksRequest(req)
-		case "blocks":
-			return findBlocksRequest(req)
-		case "validate":
-			return validateRequest(req)
-		default:
-			return clientError(http.StatusBadRequest)
+func router(req Request) (Response, error) {
+	funcLogger := logger.WithFields(log.Fields{
+		"func":    "router",
+		"mehtod":  req.HTTPMethod,
+		"payload": req.Body,
+		"isb64":   req.IsBase64Encoded,
+	})
+	funcLogger.Infof("request received")
+	// defer function will be called before function exits
+	defer funcLogger.Infof("response sent")
+
+	if req.HTTPMethod == "POST" {
+		submethod := req.QueryStringParameters["method"]
+		if err := validateSubMethod(submethod); err != nil {
+			return makeResponse(http.StatusMethodNotAllowed, err, nil)
 		}
-    default:
-        return clientError(http.StatusMethodNotAllowed)
-    }
+		payload, err := validatePayload(req.Body)
+		if err != nil {
+			return makeResponse(http.StatusBadRequest, err, nil)
+		}
+		result := kcodeHandler(payload, submethod)
+		return makeResponse(http.StatusOK, nil, result)
+	}
+	// validate or GET method will currently return just a 200 OK reponse
+	return testGetRequest(req)
 }
 
-func getError() (err error) {
-   err = nil
-   return
-}
-
-// Looking for blocks in .kcode input
-func findBlocksRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// https://docs.aws.amazon.com/lambda/latest/dg/with-on-demand-https-create-package.html#with-apigateway-example-deployment-pkg-go
-
-    body := req.Body
-	// You might want to add CORS header here:
-	// resp.Headers["Access-Control-Allow-Origin"] = "*"
-	resp := events.APIGatewayProxyResponse{Headers: make(map[string]string)}
-	resp.Headers["Access-Control-Allow-Origin"] = "*"
-	//s := fmt.Sprintf("{\"method\":\"%s\",\"body\": \"Hello World\",\"count\": 1}", method)
-	
-	err := getError()
-	
-    if err != nil {
-        return serverError(err)
-    }
-    //if resp == nil {
-    //    return clientError(http.StatusNotFound)
-    //}
-
-	verbose := false
-	filename := ""
-	kc := kcode.ExtractKcode([]byte(body))
-	_, blocks := kcode.ProcessKcodeString(filename, kc, false, true, verbose)
-	fmt.Println(blocks)
-	resp.Body = blocks[0]
-
-    js, err := json.Marshal(resp)
-    if err != nil {
-        return serverError(err)
-    }
-
-    return events.APIGatewayProxyResponse{
-        StatusCode: http.StatusOK,
-        Body:       string(js),
-    }, nil
-}
-
-// Looking for spells in .kcode input
-func findSpellsRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    body := req.Body
-	// You might want to add CORS header here:
-	resp := events.APIGatewayProxyResponse{Headers: make(map[string]string)}
-	resp.Headers["Access-Control-Allow-Origin"] = "*"
-	
-	resp.Body = body
-	
-    js, err := json.Marshal(resp)
-    if err != nil {
-        return serverError(err)
-    }
-
-    return events.APIGatewayProxyResponse{
-        StatusCode: http.StatusOK,
-        Body:       string(js),
-    }, nil
-}
-
-// Looking for spells in .kcode input
-func validateRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    body := req.Body
-	// You might want to add CORS header here:
-	// resp.Headers["Access-Control-Allow-Origin"] = "*"
-	resp := events.APIGatewayProxyResponse{Headers: make(map[string]string)}
-	resp.Headers["Access-Control-Allow-Origin"] = "*"
-
-	resp.Body = body
-
-    js, err := json.Marshal(resp)
-    if err != nil {
-        return serverError(err)
-    }
-
-    return events.APIGatewayProxyResponse{
-        StatusCode: http.StatusOK,
-        Body:       string(js),
-    }, nil
-}
-
-func testGetRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    body := req.Body
-	// You might want to add CORS header here:
-	resp := events.APIGatewayProxyResponse{Headers: make(map[string]string)}
-	resp.Headers["Access-Control-Allow-Origin"] = "*"
-	
-	/*
-	verbose := false
-	filename := ""
-	b := []byte(body)
-	_, blocks := kcode.ProcessKcodeString(filename, b, false, true, verbose)
-	fmt.Println(blocks)
-	resp.Body = blocks[0]
-	*/
-
-	resp.Body = body
-	
-    js, err := json.Marshal(resp)
-    if err != nil {
-        return serverError(err)
-    }
-
-    return events.APIGatewayProxyResponse{
-        StatusCode: http.StatusOK,
-        Body:       string(js),
-    }, nil
-}
-
-func serverError(err error) (events.APIGatewayProxyResponse, error) {
-    //var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
-	//errorLogger.Println(err.Error())
-	fmt.Println(err.Error())
-	
-    return events.APIGatewayProxyResponse{
-        StatusCode: http.StatusInternalServerError,
-        Body:       http.StatusText(http.StatusInternalServerError),
-    }, nil
-}
-
-func clientError(status int) (events.APIGatewayProxyResponse, error) {
-    return events.APIGatewayProxyResponse{
-        StatusCode: status,
-        Body:       http.StatusText(status),
-    }, nil
+func testGetRequest(req Request) (Response, error) {
+	return makeResponse(200, nil, "ok")
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"testing"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/stretchr/testify/assert"
-	"testing"
-	"fmt"
 )
 
 func TestPostBlocks(t *testing.T) {
@@ -12,12 +13,9 @@ func TestPostBlocks(t *testing.T) {
 	request := events.APIGatewayProxyRequest{
 		HTTPMethod: "POST",
 		QueryStringParameters: map[string]string{
-			"method":"validate",
+			"method": "validate",
 		},
-		Body: []byte{"source": "<xml xmlns=\"http://www.w3.org/1999/xhtml\"><variables></variables><block type=\"events_onFlick\" id=\"8e2Z`.);iVBJ-m3P/4L~\" x=\"342\" y=\"266\"><field name=\"TYPE\">up</field><statement name=\"CALLBACK\"><block type=\"objects_setColor\" id=\"z$aXI`j$V}|;^jC5jfQ+\"><value name=\"TINT\"><shadow type=\"objects_get\" id=\"@nG3U_MJv*?}?g[SyNb5\"><field name=\"ID\">all</field></shadow></value><value name=\"TO COLOR\"><shadow type=\"colour_picker\" id=\"zVP6}*(g${T/]Ln:r~c]\"><field name=\"COLOUR\">#FF5723</field></shadow></value></block></statement></block></xml>",
-			   "parts":[],
-			   "scene":"owlery",
-		},
+		Body: `{"source": "<xml xmlns="http://www.w3.org/1999/xhtml"><variables></variables><block type="events_onFlick" x="342" y="266"><field name="TYPE">up</field><statement name="CALLBACK"><block type="objects_setColor"><value name="TINT"><shadow type="objects_get"><field name="ID">all</field></shadow></value><value name="TO COLOR"><shadow type="colour_picker"><field name="COLOUR">#FF5723</field></shadow></value></block></statement></block></xml>","parts":[],"scene":"owlery"}`,
 	}
 	expectedResponse := events.APIGatewayProxyResponse{
 		StatusCode: 200,
@@ -29,7 +27,7 @@ func TestPostBlocks(t *testing.T) {
 
 	response, err := router(request)
 	fmt.Println(response)
-	
+
 	//assert.Equal(t, response.Headers, expectedResponse.Headers)
 	assert.Contains(t, response.Body, expectedResponse.Body)
 	assert.Equal(t, err, nil)
@@ -40,7 +38,7 @@ func TestGet(t *testing.T) {
 	request := events.APIGatewayProxyRequest{
 		HTTPMethod: "GET",
 		QueryStringParameters: map[string]string{
-			"method":"validate",
+			"method": "validate",
 		},
 		Body: "Something",
 	}
@@ -54,7 +52,7 @@ func TestGet(t *testing.T) {
 
 	response, err := router(request)
 	fmt.Println(response)
-	
+
 	//assert.Equal(t, response.Headers, expectedResponse.Headers)
 	assert.Contains(t, response.Body, expectedResponse.Body)
 	assert.Equal(t, err, nil)
